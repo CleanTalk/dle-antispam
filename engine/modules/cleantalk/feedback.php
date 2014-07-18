43be4fa4c76a7a5c3bdcd9b2fd2a48f106ccb249
Denis Shagimuratov <poluxster@gmail.com>
2014-07-18 11:40:49 +0600

    Fixed feedback form protection.

--- engine/modules/cleantalk/feedback.php
+++ engine/modules/cleantalk/feedback.php
@@ -5,7 +5,6 @@ if (!defined('DATALIFEENGINE')) {
 
 require_once ENGINE_DIR . '/modules/cleantalk/ct_functions.php';
 list($ct_config, $ct_config_serialized) = ct_get_config($db);
-$ct_request_id = null;
 if ($stop == '' && $ct_config['ct_enable_feedback']) {
    
     require_once ENGINE_DIR . '/modules/cleantalk/cleantalk.class.php';
@@ -38,6 +37,17 @@ if ($stop == '' && $ct_config['ct_enable_feedback']) {
     }
     $sender_ip = ($_IP == '127.0.0.1' && !empty($forwarded_for)) ? $forwarded_for : $_IP;
     
+    $ct_check_key = ct_generation_check_key();
+    $checkjs = 0;
+    if (isset($_POST['ct_checkjs']) && preg_match("/$ct_check_key/", $_POST['ct_checkjs'])) {
+        $checkjs = 1;
+    }
+
+    $ct_submit_register_time = time() - $_SESSION['ct_submit_register_time'];
+    
+    $post_info['comment_type'] = 'feedback';
+    $post_info = json_encode($post_info);
+
     $ct_request = new CleantalkRequest();
     $ct_request->auth_key = $ct_config['ct_key'];
     $ct_request->message = $message;
@@ -47,6 +57,9 @@ if ($stop == '' && $ct_config['ct_enable_feedback']) {
     $ct_request->sender_ip = $sender_ip;
     $ct_request->agent = 'dle-'.$ct_config['ct_version'];
     $ct_request->sender_info = $sender_info;
+    $ct_request->js_on = $checkjs;
+    $ct_request->submit_time = $ct_submit_register_time;
+    $ct_request->post_info = $post_info;
 
     // Принудительно отключаем проверку стоп-слов, т.к. сообщения с формы обратной связи не являются публичной информацией
     $ct_request->stoplist_check = 0;
