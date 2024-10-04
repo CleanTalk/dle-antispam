3f37fff64348affd93206f865df3a2917d37e0f4
svfcode <svfcode@mail.ru>
2024-10-04 14:00:00 +0300

    Update cleantalk domain.

--- engine/modules/cleantalk/register_template.php
+++ engine/modules/cleantalk/register_template.php
@@ -7,7 +7,7 @@ require_once ENGINE_DIR . '/modules/cleantalk/ct_functions.php';
 list($ct_config, $ct_config_serialized) = ct_get_config($db);
 
 if ($ct_config['ct_enable_mod']) {
-$tpl->copy_template .= '<br><a style="font-size: 12px; margin-left: 10px;" href="http://cleantalk.ru/dle" target="__blank">
+$tpl->copy_template .= '<br><a style="font-size: 12px; margin-left: 10px;" href="https://cleantalk.org/dle" target="__blank">
 	<b style="color: #009900;">Clean</b><b style="color: #777;">talk</b>
 	</a><br><br>';
 if (isset($ct_fill_field)) {
@@ -15,9 +15,9 @@ $tpl->copy_template .= '<script language="javascript" type="text/javascript">
 $("#name").val("'.$name.'"); $("input[name$=\'email\']").val("'.$email.'");
 </script>';
  }
-// End: cleantalk.ru
+// End: cleantalk.org
  
-// Begin: cleantalk.ru
+// Begin: cleantalk.org
 $tpl->copy_template .= "
 <script type=\"text/javascript\">
 // <![CDATA[
